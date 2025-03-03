23170b378a94415747ce86e6a52ccbf6b5ab9bcc
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-03 13:05:13 -0600

    head changes favicon and appname

--- TestMyControl-Escuela/resources/views/app.blade.php
+++ TestMyControl-Escuela/resources/views/app.blade.php
@@ -4,13 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Mycontrol') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
